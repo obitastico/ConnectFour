9dba095b0d96160b1d3d58e2d203bed7fbfbe68e
obitastico <obit-obit@web.de>
2021-08-13 13:11:58 +0200

    shoter == better

--- src/de/obitastico/connectfour/gui/ComputerThread.java
+++ src/de/obitastico/connectfour/gui/ComputerThread.java
@@ -6,12 +6,6 @@ public class ComputerThread implements Runnable {
 
     @Override
     public void run() {
-
-        SwingUtilities.invokeLater(new Runnable(){
-            @Override
-            public void run() {
-                GUI.do_turn();
-            }
-        });
+        SwingUtilities.invokeLater(GUI::do_turn);
     }
 }
